0e9b9aa5b97efe23163e9d8bbd4911f08a69a4dc
Larissa <orglary@gmail.com>
2019-09-25 18:36:30 -0300

    feat: structuring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from "react";
-import logo from "./assets/logo.svg";
+import React, { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 import "./App.scss";
 
 function App() {
+  const Content = lazy(() => importMDX("./Content.mdx"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Git Guide
-        </p>
-          {/* https://git-scm.com/book/pt-br/v1/Primeiros-passos-Configura%C3%A7%C3%A3o-Inicial-do-Git
-          …or create a new repository on the command line echo "# git-guide" >>
-          README.md git init git add README.md git commit -m "first commit" git
-          remote add origin git@github.com:RikoKami/git-guide.git git push -u
-          origin master …or push an existing repository from the command line
-          git remote add origin git@github.com:RikoKami/git-guide.git git push
-          -u origin master …or import code from another repository You can
-          initialize this repository with code from a Subversion, Mercurial, or
-          TFS project. Learn React */}
-      </header>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
     </div>
   );
 }
